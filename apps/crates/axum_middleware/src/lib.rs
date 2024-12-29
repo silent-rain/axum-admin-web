4927538b3666668389f2474f74df29aa7736be0d
silent-rain <2367221387@qq.com>
2024-12-30 00:02:39 +0800

    OpenApi权限中间件

--- apps/crates/axum_middleware/src/lib.rs
+++ apps/crates/axum_middleware/src/lib.rs
@@ -4,7 +4,7 @@ pub mod constant;
 // pub mod api_operation;
 // pub mod casbin_auth;
 pub mod cors;
-// pub mod openapi_auth;
+pub mod openapi_auth;
 pub mod system_api_auth;
 pub mod template;
 pub mod template2;
--- apps/crates/axum_middleware/src/openapi_auth.rs
+++ apps/crates/axum_middleware/src/openapi_auth.rs
@@ -1,99 +1,97 @@
 //! OpenApi权限中间件
-use std::{
-    future::{ready, Ready},
-    pin::Pin,
-    rc::Rc,
-};
+use std::{boxed::Box, convert::Infallible, task::Poll};
 
 use crate::constant::{AUTH_WHITE_LIST, OPENAPI_AUTHORIZATION, OPENAPI_PASSPHRASE};
 
-use context::{ApiAuthType, Context};
-use response::Response;
+use code::Error;
+use response::ResponseErr;
 use service_hub::{
     inject::AInjectProvider,
     user::{cached::UserCached, dto::user_base::UserPermission, UserBaseService},
 };
 
-use actix_web::{
-    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
-    web::Data,
-    Error, HttpMessage, HttpRequest,
+use axum::{
+    body::{Body, HttpBody},
+    http::Request,
+    BoxError, Extension,
 };
-use futures::Future;
+use axum_context::{ApiAuthType, Context};
+use bytes::Bytes;
+use futures::future::BoxFuture;
+use tower::{Layer, Service};
 use tracing::{error, info};
 
 /// OpenApi接口鉴权
-#[derive(Default)]
-pub struct OpenApiAuth {}
+#[derive(Clone)]
+pub struct OpenApiAuthLayer;
 
-impl<S, B> Transform<S, ServiceRequest> for OpenApiAuth
-where
-    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-    S::Future: 'static,
-    B: 'static,
-{
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type InitError = ();
-    type Transform = OpenApiAuthService<S>;
-    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+impl<S> Layer<S> for OpenApiAuthLayer {
+    type Service = OpenApiAuthService<S>;
 
-    fn new_transform(&self, service: S) -> Self::Future {
-        ready(Ok(OpenApiAuthService {
-            service: Rc::new(service),
-        }))
+    fn layer(&self, inner: S) -> Self::Service {
+        OpenApiAuthService { inner }
     }
 }
 
+#[derive(Clone)]
 pub struct OpenApiAuthService<S> {
-    service: Rc<S>,
+    inner: S,
 }
 
-impl<S, B> Service<ServiceRequest> for OpenApiAuthService<S>
+impl<S, ReqBody, ResBody> Service<Request<ReqBody>> for OpenApiAuthService<S>
 where
-    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
-    S::Future: 'static,
-    B: 'static,
+    S: Service<Request<ReqBody>, Response = axum::response::Response<ResBody>, Error = Infallible>
+        + Clone
+        + Send
+        + 'static,
+    S::Future: Send + 'static,
+    ReqBody: Send + 'static,
+    Infallible: From<<S as Service<Request<ReqBody>>>::Error>,
+    ResBody: HttpBody<Data = Bytes> + Send + 'static + From<Body>,
+    ResBody::Error: Into<BoxError>,
+    S::Error: Into<BoxError>,
 {
-    type Response = ServiceResponse<B>;
-    type Error = Error;
-    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>>>>;
+    type Response = S::Response;
+    type Error = BoxError;
+    // `BoxFuture` is a type alias for `Pin<Box<dyn Future + Send + 'a>>`
+    type Future = BoxFuture<'static, Result<Self::Response, Self::Error>>;
 
-    forward_ready!(service);
+    fn poll_ready(&mut self, cx: &mut std::task::Context<'_>) -> Poll<Result<(), Self::Error>> {
+        self.inner.poll_ready(cx).map_err(Into::into)
+    }
 
-    fn call(&self, req: ServiceRequest) -> Self::Future {
-        let service = Rc::clone(&self.service);
+    fn call(&mut self, mut req: Request<ReqBody>) -> Self::Future {
+        let not_ready_inner = self.inner.clone();
+        let mut inner = std::mem::replace(&mut self.inner, not_ready_inner);
 
-        let provider = match req.app_data::<Data<AInjectProvider>>() {
-            Some(v) => v.as_ref().clone(),
-            None => {
-                return Box::pin(async move {
-                    error!("获取服务实例失败");
-                    Err(Response::code(code::Error::InjectAproviderObj).into())
-                })
-            }
-        };
         Box::pin(async move {
-            let inner_req = req.request();
+            // 全局依赖
+            let inject_provider = match req.extensions().get::<Extension<AInjectProvider>>() {
+                Some(v) => &v.0,
+                None => {
+                    return Err(Box::new(ResponseErr::new(Error::InjectAproviderObj)))
+                        .map_err(Into::into)
+                }
+            };
 
             // 白名单放行
-            let path = req.path();
+            let path = req.uri().path();
             if AUTH_WHITE_LIST.contains(&path) {
-                let resp = service.call(req).await?;
+                let resp = inner.call(req).await?;
                 return Ok(resp);
             }
 
             // 不存在Openapi鉴权标识时, 则直接通过
             if req.headers().get(OPENAPI_AUTHORIZATION).is_none() {
-                let resp = service.call(req).await?;
+                let resp = inner.call(req).await?;
                 return Ok(resp);
             }
             // 获取 Openapi 鉴权
-            let (openapi_token, passphras) = match Self::get_openapi_token(inner_req.clone()) {
+            let (openapi_token, passphras) = match Self::get_openapi_token(&req) {
                 Ok(v) => v,
                 Err(err) => {
                     error!("获取鉴权标识失败, err: {:#?}", err);
-                    return Err(Response::err(err).into());
+                    return Err(Box::new(err)).map_err(Into::into);
                 }
             };
             // 获取缓存
@@ -112,21 +110,24 @@ where
                     permission.user_id,
                     permission.username
                 );
-                let resp = service.call(req).await?;
+                let resp = inner.call(req).await?;
                 return Ok(resp);
             }
 
             // 获取用户权限
-            let permission =
-                match Self::user_permission(provider.clone(), openapi_token.clone(), passphras)
-                    .await
-                {
-                    Ok(v) => v,
-                    Err(err) => {
-                        error!("获取权限失败, err: {:#?}", err);
-                        return Err(Response::err(err).into());
-                    }
-                };
+            let permission = match Self::user_permission(
+                inject_provider.clone(),
+                openapi_token.clone(),
+                passphras,
+            )
+            .await
+            {
+                Ok(v) => v,
+                Err(err) => {
+                    error!("获取权限失败, err: {:#?}", err);
+                    return Err(Box::new(err)).map_err(Into::into);
+                }
+            };
 
             // 设置上下文
             if let Some(ctx) = req.extensions_mut().get_mut::<Context>() {
@@ -144,7 +145,7 @@ where
             );
 
             // 响应
-            let resp = service.call(req).await?;
+            let resp = inner.call(req).await?;
             Ok(resp)
         })
     }
@@ -165,7 +166,9 @@ impl<S> OpenApiAuthService<S> {
     }
 
     /// 获取OPEN API鉴权标识Token
-    fn get_openapi_token(req: HttpRequest) -> Result<(String, String), code::ErrorMsg> {
+    fn get_openapi_token<ReqBody>(
+        req: &Request<ReqBody>,
+    ) -> Result<(String, String), code::ErrorMsg> {
         let token = req
             .headers()
             .get(OPENAPI_AUTHORIZATION)
